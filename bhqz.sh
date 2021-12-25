b98d8fdeaeb201fee43ca748d3602d14356957e8
caoqiang <caoqiang@jiuqi.com.cn>
2021-12-25 19:33:54 +0800

    up

--- bhqz.sh
+++ bhqz.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+ShellDir=${JD_DIR:-$(
+    cd $(dirname $0)
+    pwd
+)}
+
+git clone https://github.com/bhqz/bhqz.git bhqz
+cd bhqz
 # 需要检查的文件
 checkFiles=$(git log --since='date -d "yesterday" +%Y.%m.%d' --name-only --pretty=format:"")
 git checkout -- ${checkFiles}
@@ -8,11 +15,11 @@ for file in ${checkFiles}; do
     echo $file
     extension="${file##*.}"
     if [ $extension == "txt" ];then
-        mv ${file} "bhqz.txt"
+        mv ${file} ${ShellDir}/bhqz.txt
     fi
 
     if [ $extension == "yaml" ];then
-        mv ${file} "bhqz.yml"
+        mv ${file} ${ShellDir}/bhqz.yml
     fi
-
 done
+cd ${ShellDir} && bhqz
