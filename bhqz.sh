8193fb70969e9c0591aa8fcba6b986807f8484dd
caoqiang <caoqiang@jiuqi.com.cn>
2021-12-27 09:53:18 +0800

    up

--- bhqz.sh
+++ bhqz.sh
@@ -9,7 +9,7 @@ git clone https://github.com/bhqz/bhqz.git bhqz
 cd bhqz
 # 需要检查的文件
 checkFiles=$(git log --since='date -d "yesterday" +%Y.%m.%d' --name-only --pretty=format:"")
-git checkout -- ${checkFiles}
+# git checkout -- ${checkFiles}
 echo "bhqz path"
 pwd
 echo "checkout files"
